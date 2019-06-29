c3c1bd46067ce3236d484b099169c380160e6018
Jurian <j.vink@gdksoftware.nl>
2019-06-29 16:11:02 +0200

    heroku

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const express = require('express');
 const app = express();
 
 app.listen(process.env.PORT, () =>
-  console.log(`Star gazing using port ${process.env.PORT}`),
+    console.log(`Star gazing using port ${process.env.PORT}`),
 );
 
 con.connect((error) => {
     if (error) throw error;
 
-    con.query(`SELECT * FROM planet`, (error, result) => {
-        if (error) throw error;
+    app.get('/planets', (req, res) => {
+        res.setHeader(`Access-Control-Allow-Origin`, `${process.env.ADDRESS}:${process.env.PORT}`);
+        res.setHeader(`Access-Control-Allow-Methods`, `GET, POST, OPTIONS, PUT, PATCH, DELETE`);
+        res.setHeader(`Access-Control-Allow-Headers`, `X-Requested-With,content-type`);
 
-        app.get('/', (req, res) => {
+        con.query(`SELECT * FROM planet`, (error, result) => {
+            if (error) throw error;
             res.send(result);
         });
     });
